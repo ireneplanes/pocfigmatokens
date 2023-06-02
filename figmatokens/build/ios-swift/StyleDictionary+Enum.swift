ff233778b89cf48e8c9ebb21b09847d1755ea7ba
Laura <laura-ruiz.sanchez@external.telekom.de>
2023-06-02 11:02:48 +0200

    tokens build

--- figmatokens/build/ios-swift/StyleDictionary+Enum.swift
+++ figmatokens/build/ios-swift/StyleDictionary+Enum.swift
@@ -4,36 +4,36 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 02 Jun 2023 08:41:21 GMT
+// Generated on Fri, 02 Jun 2023 09:00:49 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryEnum {
-    public static let stylesBorderRadiusBorderRadius1 = 6px
-    public static let stylesBorderRadiusBorderRadius2 = 15px
+    public static let stylesBorderRadiusBorderRadius1 = 15px
+    public static let stylesBorderRadiusBorderRadius2 = 6px
     public static let stylesBorderStyleBorder1 = [object Object]
     public static let stylesBorderStyleBorder2 = [object Object]
-    public static let stylesColorsOpacityOpacity1 = 0.5
-    public static let stylesColorsOpacityOpacity2 = 0.8
+    public static let stylesColorsOpacityOpacity1 = 0.8
+    public static let stylesColorsOpacityOpacity2 = 0.5
     public static let stylesColorsPrimaryBlack = #191919
-    public static let stylesColorsPrimaryGrey1 = #B2B2B2
-    public static let stylesColorsPrimaryGrey2 = #f2f2f2
-    public static let stylesColorsPrimaryMagenta = #E20074
+    public static let stylesColorsPrimaryGrey1 = #e0dfdf
+    public static let stylesColorsPrimaryGrey2 = #f0f0f0
+    public static let stylesColorsPrimaryMagenta = #c10466
     public static let stylesColorsPrimaryWhite = #ffffff
-    public static let stylesColorsSecondaryGrey3 = #EAEAEA
+    public static let stylesColorsSecondaryGrey3 = #e0dfdf
     public static let stylesColorsSecondaryGrey4 = #F6F6F6
     public static let stylesColorsSecondaryRed = #D82A48
-    public static let stylesFontsFontCaseCase1 = capitalize
-    public static let stylesFontsFontCaseCase2 = uppercase
-    public static let stylesFontsFontFamilyPrimary = TeleNeo
-    public static let stylesFontsFontFamilySecondary = Inter
-    public static let stylesFontsFontSizeFontsize1 = 12px
-    public static let stylesFontsFontSizeFontsize2 = 14px
-    public static let stylesFontsFontSizeFontsize3 = 16px
-    public static let stylesFontsFontWeightWeight1 = 400
-    public static let stylesFontsFontWeightWeight2 = 500
-    public static let stylesFontsFontWeightWeight3 = 800
+    public static let stylesFontsFontCaseCase1 = uppercase
+    public static let stylesFontsFontCaseCase2 = capitalize
+    public static let stylesFontsFontFamilyPrimary = Inter
+    public static let stylesFontsFontFamilySecondary = TeleNeo
+    public static let stylesFontsFontSizeFontsize1 = 14px
+    public static let stylesFontsFontSizeFontsize2 = 16px
+    public static let stylesFontsFontSizeFontsize3 = 18px
+    public static let stylesFontsFontWeightWeight1 = 500
+    public static let stylesFontsFontWeightWeight2 = 800
+    public static let stylesFontsFontWeightWeight3 = 400
     public static let stylesIconsAdd = fa-solid fa-plus
     public static let stylesIconsExport = fas fa-file-export
 }
--- figmatokens/build/ios-swift/StyleDictionary+Struct.swift
+++ figmatokens/build/ios-swift/StyleDictionary+Struct.swift
@@ -4,36 +4,36 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 02 Jun 2023 08:41:21 GMT
+// Generated on Fri, 02 Jun 2023 09:00:49 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let stylesBorderRadiusBorderRadius1 = 6px
-    internal static let stylesBorderRadiusBorderRadius2 = 15px
+    internal static let stylesBorderRadiusBorderRadius1 = 15px
+    internal static let stylesBorderRadiusBorderRadius2 = 6px
     internal static let stylesBorderStyleBorder1 = [object Object]
     internal static let stylesBorderStyleBorder2 = [object Object]
-    internal static let stylesColorsOpacityOpacity1 = 0.5
-    internal static let stylesColorsOpacityOpacity2 = 0.8
+    internal static let stylesColorsOpacityOpacity1 = 0.8
+    internal static let stylesColorsOpacityOpacity2 = 0.5
     internal static let stylesColorsPrimaryBlack = #191919
-    internal static let stylesColorsPrimaryGrey1 = #B2B2B2
-    internal static let stylesColorsPrimaryGrey2 = #f2f2f2
-    internal static let stylesColorsPrimaryMagenta = #E20074
+    internal static let stylesColorsPrimaryGrey1 = #e0dfdf
+    internal static let stylesColorsPrimaryGrey2 = #f0f0f0
+    internal static let stylesColorsPrimaryMagenta = #c10466
     internal static let stylesColorsPrimaryWhite = #ffffff
-    internal static let stylesColorsSecondaryGrey3 = #EAEAEA
+    internal static let stylesColorsSecondaryGrey3 = #e0dfdf
     internal static let stylesColorsSecondaryGrey4 = #F6F6F6
     internal static let stylesColorsSecondaryRed = #D82A48
-    internal static let stylesFontsFontCaseCase1 = capitalize
-    internal static let stylesFontsFontCaseCase2 = uppercase
-    internal static let stylesFontsFontFamilyPrimary = TeleNeo
-    internal static let stylesFontsFontFamilySecondary = Inter
-    internal static let stylesFontsFontSizeFontsize1 = 12px
-    internal static let stylesFontsFontSizeFontsize2 = 14px
-    internal static let stylesFontsFontSizeFontsize3 = 16px
-    internal static let stylesFontsFontWeightWeight1 = 400
-    internal static let stylesFontsFontWeightWeight2 = 500
-    internal static let stylesFontsFontWeightWeight3 = 800
+    internal static let stylesFontsFontCaseCase1 = uppercase
+    internal static let stylesFontsFontCaseCase2 = capitalize
+    internal static let stylesFontsFontFamilyPrimary = Inter
+    internal static let stylesFontsFontFamilySecondary = TeleNeo
+    internal static let stylesFontsFontSizeFontsize1 = 14px
+    internal static let stylesFontsFontSizeFontsize2 = 16px
+    internal static let stylesFontsFontSizeFontsize3 = 18px
+    internal static let stylesFontsFontWeightWeight1 = 500
+    internal static let stylesFontsFontWeightWeight2 = 800
+    internal static let stylesFontsFontWeightWeight3 = 400
     internal static let stylesIconsAdd = fa-solid fa-plus
     internal static let stylesIconsExport = fas fa-file-export
 }
